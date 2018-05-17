caca305909f500493f0cfee563ad43eaca15445e
rafal <rafal@jakubowski>
2018-05-17 13:45:35 +0200

    added interface

---
+++ src/main/java/edu/kis/vh/nursery/stack/Stackable.java
@@ -0,0 +1,10 @@
+package edu.kis.vh.nursery.stack;
+
+public interface Stackable {
+	public int getTotal();
+	public void countIn(int in);
+	public boolean callCheck();
+	public boolean isFull();
+	public int peekaboo();
+	public int countOut();	
+}
